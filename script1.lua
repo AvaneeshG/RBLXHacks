874db0a07f9919d7d23687809f2ca1b00d0da8ee
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-27 21:47:58 +0530

    uobvbuvur

--- script1.lua
+++ script1.lua
@@ -1,1 +1,28 @@
-print("everything is working~")
+-- vscode beatuify
+-- tap tap games
+-- FFFFFFFFFFFF    U         U           CCCCCCCCCCC      K         KK
+-- F               U         U          C                 K       KK
+-- F               U         U         C                  K    KK
+-- FFFFFFFF        U         U         C                  KKKKK
+-- F               U         U         C                  KKKKK 
+-- F                U       U          C                  K    KK
+-- F                U       U          C                  K      KK
+-- F                U       U           C                 K        KK
+-- F                 UUUUUUU             CCCCCCCCCC       K          KK
+_G.at = true;
+while _G.at == true do
+    game:GetService("ReplicatedStorage").Events.Client.emitClicks:FireServer()
+
+    local args = {
+        [1] = {
+            ["manual"] = {
+                ["1"] = 1
+            }
+        }
+    }
+
+    game:GetService("ReplicatedStorage").Clicker2:InvokeServer(unpack(args))
+
+
+    wait()
+end
