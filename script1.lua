ff91aa16283645e698bb0ea068b684c5ef36a57e
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-27 22:08:01 +0530

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,14 +1,6 @@
 -- vscode beatuify
 -- tap tap games
--- FFFFFFFFFFFF    U         U           CCCCCCCCCCC      K         KK
--- F               U         U          C                 K       KK
--- F               U         U         C                  K    KK
--- FFFFFFFF        U         U         C                  KKKKK
--- F               U         U         C                  KKKKK 
--- F                U       U          C                  K    KK
--- F                U       U          C                  K      KK
--- F                U       U           C                 K        KK
--- F                 UUUUUUU             CCCCCCCCCC       K          KK
+
 _G.at = true;
 while _G.at == true do
     local args = {[1] = {["manual"] = {["1"]=1}}}
