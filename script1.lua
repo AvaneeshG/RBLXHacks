3ce31b988553458ee100c153a2bf52e3d103cea2
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-27 22:05:11 +0530

    ddd

--- script1.lua
+++ script1.lua
@@ -11,8 +11,6 @@
 -- F                 UUUUUUU             CCCCCCCCCC       K          KK
 _G.at = true;
 while _G.at == true do
-    game:GetService("ReplicatedStorage").Events.Client.emitClicks:FireServer()
-
     local args = {[1] = {["manual"] = {["1"]=1}}}
     game:GetService("ReplicatedStorage").Clicker2:InvokeServer(unpack(args))
 
