793d99b002c1151082c2fb325ce178fe1852f159
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-27 22:37:50 +0530

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -3,9 +3,7 @@
 
 _G.at = true;
 while _G.at == true do
-    local args = {[1] = {["manual"] = {["1"]=1}}}
-    game:GetService("ReplicatedStorage").Clicker2:InvokeServer(unpack(args))
-
-
+    --tested on https://web.roblox.com/games/6512923934/x750-000-000-Clicks-Ultra-Clickers
+    workspace.Events.AddClick:FireServer()
     wait()
 end
