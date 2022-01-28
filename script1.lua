01d5d942fd633e92cf1475a7e4809ac599715235
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-28 21:54:04 +0530

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -4,8 +4,14 @@
 _G.at = true;
 spawn(function() 
     while _G.at == true do 
-    local args = {[1] = {["manual"] = {["496637"]=700}}}
-    game:GetService("ReplicatedStorage").Clicker2:InvokeServer(unpack(args))
-    wait(); 
+        game:GetService("ReplicatedStorage").Events.Client.emitClicks:FireServer()
+        local args = {[1] = {["manual"] = {["496637"]=1}}}
+        game:GetService("ReplicatedStorage").Clicker2:InvokeServer(unpack(args))
+        wait(); 
     end 
+end)
+
+spawn(function() 
+    local args = {[1]=workspace.Eggs:FindFirstChild("Sun God"),[2]=false,[3]=false}
+    game:GetService("ReplicatedStorage").Events.Client.purchaseEgg2:InvokeServer(unpack(args))
 end)
