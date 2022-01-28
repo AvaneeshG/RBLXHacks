fca64d3ba789f12f5bf48a57c36868c32f39fbc1
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-28 20:55:27 +0530

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -2,8 +2,10 @@
 -- tap tap games
 
 _G.at = true;
-while _G.at == true do
-    --tested on https://web.roblox.com/games/6512923934/x750-000-000-Clicks-Ultra-Clickers
-    workspace.Events.AddClick:FireServer()
-    wait()
-end
+spawn(function() 
+    while _G.at == true do
+        --tested on https://web.roblox.com/games/6512923934/x750-000-000-Clicks-Ultra-Clickers
+        workspace.Events.AddClick:FireServer()
+        wait()
+    end
+end)
