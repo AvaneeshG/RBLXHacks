764720fa1b711222735a29d84a5d89a0450a9259
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-01-28 20:57:00 +0530

    one line code

--- script1.lua
+++ script1.lua
@@ -1,11 +1,4 @@
 -- vscode beatuify
 -- tap tap games
 
-_G.at = true;
-spawn(function() 
-    while _G.at == true do
-        --tested on https://web.roblox.com/games/6512923934/x750-000-000-Clicks-Ultra-Clickers
-        workspace.Events.AddClick:FireServer()
-        wait()
-    end
-end)
+_G.at = true; spawn(function() while _G.at == true do workspace.Events.AddClick:FireServer(); wait(); end end)
